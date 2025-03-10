94290535590ae81cc5d1ff2893b4b9eba25276f2
Ethan Lewis <ethanlewis@macbookpro.mynetworksettings.com>
2025-03-10 13:09:07 -0400

    Corrected routes.py import

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 from flask import render_template
-from . import app
+from __init__ import app
 
-# Route for the homepage
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route for the 'about' page
 @app.route('/about')
 def about():
     return render_template('about.html')
